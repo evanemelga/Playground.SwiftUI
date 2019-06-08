93ca934d9fcea7e4f63575c9750f03d78917fe5d
Evan Emelga <evanemelga@hotmail.com>
2019-06-08 18:24:27 +0700

    Initial Commit

---
+++ SwiftUITrial/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITrial
+//
+//  Created by Evan Emelga on 08/06/19.
+//  Copyright © 2019 Evan Emelga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
